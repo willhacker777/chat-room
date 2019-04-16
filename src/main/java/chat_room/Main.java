9ebd74eff4c67e9fd353975eae183025c9028c62
will <willhacker777@gmail.com>
2019-04-16 03:35:27 -0700

    config gradle to build jar; use jetty to serve static html

--- src/main/java/chat_room/Main.java
+++ src/main/java/chat_room/Main.java
@@ -1,10 +1,17 @@
 package chat_room;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(8080);
+        ResourceHandler resourceHandler= new ResourceHandler();
+        resourceHandler.setResourceBase("static/index.html");
+        ContextHandler contextHandler= new ContextHandler("/");
+        contextHandler.setHandler(resourceHandler);
+        server.setHandler(contextHandler);
         try {
             server.start();
             server.join();
